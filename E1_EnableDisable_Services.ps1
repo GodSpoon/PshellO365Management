ece842ec10fcba81615a8a5eaee1155e91294929
GodSpoon <60049225+GodSpoon@users.noreply.github.com>
2021-07-16 16:33:01 -0400

    Uploaded v1.0 Scripts
    These scripts detail how to apply licensing changes for all users in an Azure AD environment

---
+++ E1_EnableDisable_Services.ps1
@@ -0,0 +1,26 @@
+# Disables the listed plans in SKU STANDARDPACK 
+
+# Install MSOnline if needed from PSGallery
+Install-Module MSOnline
+
+# Connect to MS Online
+Connect-MsolService
+
+# Create license object. After "-DisabledPlans" tag list all plans you want disabled on account(s)
+$LO = New-MsolLicenseOptions -AccountSkuId "pathfinderservices:STANDARDPACK" -DisabledPlans POWER_VIRTUAL_AGENTS_O365_P1,KAIZALA_O365_P2,Deskless,FLOW_O365_P1,POWERAPPS_O365_P1,SWAY,YAMMER_ENTERPRISE,MCOSTANDARD
+
+# Import a .csv list of users and create an object for it (a row needs to be named "UserPrincipalName" and contain the principal names)
+# (You can download from the O365 admin center)
+$MyUsers = Import-Csv -Path "C:/myusers.csv"
+
+# Apply change to all users in .csv file
+foreach($user in $MyUsers){
+Set-MsolUserLicense -UserPrincipalName $user.UserPrincipalName `
+    -LicenseOptions $LO
+	}
+	
+	
+#This script does not assign licensing to an account, if an account in the .csv file is not licensed, it will error out
+
+#To see assigned licenses for a specific user (Replace <UPN> with a user)
+#(Get-MsolUser -UserPrincipalName <UPN>).Licenses.ServiceStatus}
